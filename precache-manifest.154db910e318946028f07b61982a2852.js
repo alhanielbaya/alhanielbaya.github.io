dfc25be074e721fa150994c4f37ee8dcbb3d97ca
alhanielbaya <alstrom201@gmail.com>
2020-02-06 17:34:17 -0800

    first commit

---
+++ precache-manifest.154db910e318946028f07b61982a2852.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4572139da96d5a1c47722efbe0915688",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d925fee686d40f2035f",
+    "url": "/static/css/2.059f48f8.chunk.css"
+  },
+  {
+    "revision": "f5fa8974b34ed8d8f295",
+    "url": "/static/css/main.d8a03ff9.chunk.css"
+  },
+  {
+    "revision": "8d925fee686d40f2035f",
+    "url": "/static/js/2.f66b8383.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.f66b8383.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5fa8974b34ed8d8f295",
+    "url": "/static/js/main.8d8dc84c.chunk.js"
+  },
+  {
+    "revision": "ef0dbfb88bc9d7d1dc78",
+    "url": "/static/js/runtime-main.c929788d.js"
+  }
+]);
